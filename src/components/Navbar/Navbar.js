30a91fdf424fb69b8bf32a5f06c668c302d6cdc7
Vasco Gomes <vasco.gomes2000@gmail.com>
2023-10-29 15:58:55 +0000

    Navbar Working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,16 +5,15 @@ class Navbar extends Component {
     return (
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <a class="nav-link active" aria-current="page" href="/">Friends List</a>
+            <a class="nav-link" href="/addFriend">Add Friend</a>
+            <a class="nav-link" href="/updateFriend">Update Friend</a>
+            <a class="nav-link" href="/deleteFriend">Delete Friend</a>
           </div>
         </div>
       </div>
